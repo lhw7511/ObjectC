e67c68b6fde2e1a5e8fe796d159c9f5d173eca70
hyunwooklee <lhw751@naver.com>
2022-04-18 13:39:39 +0900

    getter setter 자동 생성 property

--- ObjectC/main.m
+++ ObjectC/main.m
@@ -13,33 +13,39 @@
     int seats;
 }
 //member method
+//getter setter 를 수동으로 선언해주지 않아도 자동으로 만들어줌(java lombok과 유사)
+@property int wheels;
+@property int seats;
 //setter
--(void)setWheels:(int)w;
--(void)setSeats:(int)s;
+//-(void)setWheels:(int)w;
+//-(void)setSeats:(int)s;
 //getter
 //objective c 에서 getter명은 변수명과 동일하게 쓰는것이 권장됨.
--(int)wheels;
--(int)seats;
+//-(int)wheels;
+//-(int)seats;
 -(void)print;
 @end
 
 @implementation Vehicle
--(void)setWheels:(int)w{
-    wheels = w;
-}
--(void)setSeats:(int)s{
-    seats = s;
-}
+//-(void)setWheels:(int)w{
+//    wheels = w;
+//}
+//-(void)setSeats:(int)s{
+//    seats = s;
+//}
+//안해도 되지만 자동완성기능때문에 함.
+@synthesize  wheels;
+@synthesize  seats;
 -(void)print{
     NSLog(@"wheels : %i, seats : %i",wheels,seats);
 }
 
--(int)seats{
-    return seats;
-}
--(int)wheels{
-    return wheels;
-}
+//-(int)seats{
+//    return seats;
+//}
+//-(int)wheels{
+//    return wheels;
+//}
 
 
 @end
